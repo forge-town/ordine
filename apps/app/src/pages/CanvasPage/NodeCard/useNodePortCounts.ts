69008acb57742c71f1d8b03ceb93767c4498bd43
Zander Zhang <276678881@qq.com>
2026-05-10 23:16:13 +0800

    refactor: remove unused selectNodePortCounts function and related imports

--- apps/app/src/pages/CanvasPage/NodeCard/useNodePortCounts.ts
+++
@@ -1,5 +0,0 @@
-import type { HarnessCanvasState } from "../_store/harnessCanvasStore";
-import { getNodePortCounts } from "./nodePorts";
-
-export const selectNodePortCounts = (nodeId: string) => (state: HarnessCanvasState) =>
-  getNodePortCounts(state.edges, nodeId, state.connectStart);
