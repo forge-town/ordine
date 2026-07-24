de4a9bf67c488eb810b3536fb31360670250d1f0
Alan-Youngzhe <121435445@qq.com>
2026-07-24 16:23:50 +0800

    fix(views): 修复 check-types 类型错误
    - tsconfig.check.json 加入 vite/client 类型
    - 为 test-wrapper 的 mock data provider 添加泛型参数
    - package.json devDependencies 添加 vite

--- packages/views/src/test/test-wrapper.tsx
+++ packages/views/src/test/test-wrapper.tsx
@@ -1,4 +1,12 @@
-import { Refine, type DataProvider } from "@refinedev/core";
+import {
+  Refine,
+  type BaseRecord,
+  type CreateParams,
+  type DataProvider,
+  type DeleteOneParams,
+  type GetOneParams,
+  type UpdateParams,
+} from "@refinedev/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render as rtlRender, type RenderOptions } from "@testing-library/react";
 import { PlatformProvider, type PlatformCapabilities } from "../platform";
@@ -16,10 +24,25 @@ const createTestQueryClient = () =>
 export const canvasTestDataProvider: DataProvider = {
   getList: async () => ({ data: [], total: 0 }),
   getMany: async () => ({ data: [] }),
-  getOne: async ({ id }) => ({ data: { id } }),
-  create: async ({ variables }) => ({ data: variables as { id: string } }),
-  update: async ({ id, variables }) => ({ data: { id, ...variables } }),
-  deleteOne: async ({ id }) => ({ data: { id } }),
+  getOne: async <TData extends BaseRecord>({ id }: GetOneParams) => ({
+    data: { id } as TData,
+  }),
+  create: async <TData extends BaseRecord, TVariables>({
+    variables,
+  }: CreateParams<TVariables>) => ({
+    data: { id: "1", ...variables } as unknown as TData,
+  }),
+  update: async <TData extends BaseRecord, TVariables>({
+    id,
+    variables,
+  }: UpdateParams<TVariables>) => ({
+    data: { id, ...variables } as unknown as TData,
+  }),
+  deleteOne: async <TData extends BaseRecord, TVariables>({
+    id,
+  }: DeleteOneParams<TVariables>) => ({
+    data: { id } as TData,
+  }),
   getApiUrl: () => "http://localhost:9433/api",
 };
 
